56f312985509577edcbe3644b2be55759c74b0f7
drericstrong <dr.eric.strong@gmail.com>
2017-03-03 19:44:48 -0500

    Fixing documentation
    Mocking some modules to fix automatic documentation generation on
    readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,18 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 
+import shlex
+
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['matplotlib', 'numpy', 'pandas', 'seaborn', 'scipy', 'scikit-fuzzy', 'skfuzzy', 'scikit-learn', 'sklearn', 'sympy', 'statsmodels', 'pyedna']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # -- General configuration ------------------------------------------------
 
